ee197b8e00eacfd363bb2bf56992cb79e7ab15d3
Joshua Bachmeier <uwdkl@student.kit.edu>
2019-10-21 17:37:17 +0200

    Make backend import python < 3.7 compatible

--- ldf_adapter/backend/__init__.py
+++ ldf_adapter/backend/__init__.py
@@ -2,14 +2,22 @@ import sys
 
 from ..config import CONFIG
 
-def __getattr__(name):
-    """Return the `User` and `Group` from the configured backend."""
-    __backend__ = f"{__name__}.{CONFIG['ldf_adapter']['backend']}"
-    __import__(__backend__)
+__backend__ = f"{__name__}.{CONFIG['ldf_adapter']['backend']}"
+__import__(__backend__)
 
-    if name == 'User':
-        return getattr(sys.modules[__backend__], 'User')
-    elif name == 'Group':
-        return getattr(sys.modules[__backend__], 'Group')
-    else:
-        raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+class Backend:
+    """Need to put __getattr__ in a classe for Python < 3.7 compatibility.
+
+    See https://stackoverflow.com/questions/2447353/getattr-on-a-module#7668273
+    """
+
+    def __getattr__(self, name):
+        """Return the `User` and `Group` from the configured backend."""
+        if name == 'User':
+            return getattr(sys.modules[__backend__], 'User')
+        elif name == 'Group':
+            return getattr(sys.modules[__backend__], 'Group')
+        else:
+            raise AttributeError(f"backend module '{__name__}' has no attribute '{name}'")
+
+sys.modules[__name__] = Backend()
