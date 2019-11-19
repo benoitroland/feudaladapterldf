f7c10c1effd3522f925deff3cfda130a4c8687b7
Joshua Bachmeier <uwdkl@student.kit.edu>
2019-11-19 11:41:28 +0100

    Add (failing) integration tests to document issues with inputs from different IDPs

--- tests/ldf_adapter/__init__.py
+++ tests/ldf_adapter/__init__.py
@@ -129,3 +129,221 @@ class UserInfoTest(TestCase):
         for raw,cooked in isss:
             info = UserInfo({'user': {'userinfo': {}}})
             self.assertEqual(info._group_masked_for_bwidm(raw), cooked)
+
+
+class UserInfoIntegration(TestCase):
+    def assert_name(self, info):
+        self.assertEqual(info.given_name, "Marcus")
+        self.assertEqual(info.family_name, "Hardt")
+        self.assertEqual(info.full_name, "Marcus Hardt")
+
+    def test_unity(self):
+        input = {
+            "display_name": "Marcus Hardt",
+            "eduperson_assurance": [
+                "https://refeds.org/assurance/IAP/medium",
+                "https://refeds.org/assurance/IAP/local-enterprise",
+                "https://refeds.org/assurance/ID/eppn-unique-no-reassign",
+                "https://refeds.org/assurance/ATP/ePA-1m",
+                "https://refeds.org/assurance/ATP/ePA-1d",
+                "https://refeds.org/assurance/ID/unique",
+                "https://refeds.org/assurance/profile/cappuccino",
+                "https://refeds.org/assurance/IAP/low"
+            ],
+            "eduperson_entitlement": [
+                "urn:geant:h-df.de:group:IMK-TRO-EWCC#login.helmholtz-data-federation.de",
+                "urn:geant:h-df.de:group:MyExampleColab#login.helmholtz-data-federation.de",
+                "urn:geant:h-df.de:group:wlcg-test#login.helmholtz-data-federation.de",
+                "urn:geant:h-df.de:group:HDF#login.helmholtz-data-federation.de"
+            ],
+            "eduperson_principal_name": "lo0018@kit.edu",
+            "eduperson_scoped_affiliation": "member@kit.edu",
+            "eduperson_unique_id": "6c611e2a2c1c487f9948c058a36c8f0e@login.helmholtz-data-federation.de",
+            "email": "marcus.hardt@kit.edu",
+            "email_verified": "true",
+            "family_name": "Hardt",
+            "given_name": "Marcus",
+            "groups": [
+                "/wlcg-test",
+                "/IMK-TRO-EWCC",
+                "/MyExampleColab",
+                "/HDF",
+                "/"
+            ],
+            "name": "Marcus Hardt",
+            "preferred_username": "marcus",
+            "sn": "Hardt",
+            "ssh_key": "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqA5FW6m3FbFhCOsRQBxKMRki5qJxoNhZdaeLXg6ym/ marcus@nemo2019\n",
+            "sub": "6c611e2a-2c1c-487f-9948-c058a36c8f0e"
+        }
+
+        info = UserInfo({'user': {'userinfo': input}})
+        self.assert_name(info)
+        self.assertEqual(info.username, "marcus")
+        self.assertEqual(info.email, "marcus.hardt@kit.edu")
+        self.assertEqual(sorted(info.groups), sorted(["imk-tro-ewcc", "my_example_colab", "wlcg-test", "hdf"]))
+
+        self.assertEqual(info.unique_id,
+                         "6c611e2a2c1c487f9948c058a36c8f0e@login.helmholtz-data-federation.de")
+        self.assertEqual(info.eppn, "lo0018@kit.edu")
+
+        self.assertTrue(info.assurance.profile.is_cappuccino())
+
+
+    input_egi = {
+        "acr": "https://aai.egi.eu/LoA#Substantial",
+        "eduperson_assurance": [
+            "https://aai.egi.eu/LoA#Substantial"
+        ],
+        "email": "marcus.hardt@kit.edu",
+        "family_name": "Hardt",
+        "given_name": "Marcus",
+        "preferred_username": "mhardt",
+        "sub": "d7a53cbe3e966c53ac64fde7355956560282158ecac8f3d2c770b474862f4756@egi.eu"
+    }
+
+    def test_egi(self):
+        info = UserInfo({'user': {'userinfo': self.input_egi}})
+        self.assert_name(info)
+        self.assertEqual(info.username, "mhardt")
+        self.assertEqual(info.email, "marcus.hardt@kit.edu")
+
+    def test_egi_id(self):
+        info = UserInfo({'user': {'userinfo': self.input_egi}})
+        self.assertEqual(info.unique_id, "d7a53cbe3e966c53ac64fde7355956560282158ecac8f3d2c770b474862f4756@egi.eu")
+        self.assertEqual(info.eppn, info.unique_id)
+
+    def test_egi_assurance(self):
+        info = UserInfo({'user': {'userinfo': self.input_egi}})
+        self.assertIsNotNone(info.assurance.profile)
+
+    def test_egi_has_groups(self):
+        info = UserInfo({'user': {'userinfo': self.input_egi}})
+        self.assertTrue(info.groups)
+
+    def test_egi_sub_is_unscoped(self):
+        info = UserInfo({'user': {'userinfo': self.input_egi}})
+        self.assertIn('@', info.userinfo['sub'])
+
+
+    input_deep_iam = {
+        "external_authn": {
+            "iss": "https://accounts.google.com",
+            "sub": "104223951181002749851",
+            "type": "oidc"
+        },
+        "family_name": "Hardt",
+        "given_name": "Marcus",
+        "groups": [
+            "KIT-Cloud"
+        ],
+        "name": "Marcus Hardt",
+        "organisation_name": "deep-hdc",
+        "preferred_username": "marcus",
+        "sub": "d9730f60-3b19-4f45-83ab-f29addf72d58",
+        "updated_at": "Mon Jun 25 16:55:15 CEST 2018"
+    }
+
+    def test_deep_iam(self):
+        info = UserInfo({'user': {'userinfo': self.input_deep_iam}})
+        self.assert_name(info)
+        self.assertEqual(info.username, "marcus")
+        self.assertEqual(sorted(info.groups), sorted(["kit-cloud"]))
+
+    def test_deep_iam_id(self):
+        info = UserInfo({'user': {'userinfo': self.input_egi}})
+        self.assertEqual(info.unique_id, "d9730f60-3b19-4f45-83ab-f29addf72d58@deep-hdc")
+        self.assertEqual(info.eppn, info.unique_id)
+
+
+    input_indigo_iam = {
+        "external_authn": {
+            "iss": "https://accounts.google.com",
+            "sub": "104223951181002749851",
+            "type": "oidc"
+        },
+        "family_name": "Hardt",
+        "gender": "M",
+        "given_name": "Marcus",
+        "groups": [
+            "Users",
+            "Developers",
+            "test.vo-users"
+        ],
+        "name": "Marcus Hardt",
+        "organisation_name": "indigo-dc",
+        "preferred_username": "marcus",
+        "sub": "a1ea3aa2-8daf-41bb-b4fb-eb88f439e446",
+        "updated_at": 1563283972
+    }
+
+    def test_indigo_iam(self):
+        info = UserInfo({'user': {'userinfo': self.input_indigo_iam}})
+        self.assert_name(info)
+        self.assertEqual(info.username, "marcus")
+        self.assertEqual(sorted(info.groups), sorted(["users", "developers", "test-vo-users"]))
+
+    def test_indigo_iam_id(self):
+        info = UserInfo({'user': {'userinfo': self.input_indigo_iam}})
+        self.assertEqual(info.unique_id, "d9730f60-3b19-4f45-83ab-f29addf72d58@indigo-dc")
+        self.assertEqual(info.eppn, info.unique_id)
+
+
+    input_kit = {
+        "displayName": "Hardt, Marcus (SCC)",
+        "eduPersonEntitlement": [
+            "urn:geant:kit.edu:group:DFN-SLCS",
+            "urn:geant:kit.edu:group:LSDF-DIS",
+            "urn:geant:kit.edu:group:bwGrid",
+            "urn:geant:kit.edu:group:bwLSDF-FS",
+            "urn:geant:kit.edu:group:bwUniCluster",
+            "urn:geant:kit.edu:group:bwsyncnshare",
+            "urn:geant:kit.edu:group:bwsyncnshare-idm",
+            "urn:geant:kit.edu:group:gruppenverwalter"
+        ],
+        "eduPersonPrincipalName": "lo0018@kit.edu",
+        "eduPersonScopedAffiliation": [
+            "employee@kit.edu",
+            "member@kit.edu"
+        ],
+        "eduperson_entitlement": [
+            "urn:geant:kit.edu:group:DFN-SLCS",
+            "urn:geant:kit.edu:group:LSDF-DIS",
+            "urn:geant:kit.edu:group:bwGrid",
+            "urn:geant:kit.edu:group:bwLSDF-FS",
+            "urn:geant:kit.edu:group:bwUniCluster",
+            "urn:geant:kit.edu:group:bwsyncnshare",
+            "urn:geant:kit.edu:group:bwsyncnshare-idm",
+            "urn:geant:kit.edu:group:gruppenverwalter"
+        ],
+        "eduperson_principal_name": "lo0018@kit.edu",
+        "eduperson_scoped_affiliation": [
+            "employee@kit.edu",
+            "member@kit.edu"
+        ],
+        "email": "marcus.hardt@kit.edu",
+        "family_name": "Hardt",
+        "givenName": "Marcus",
+        "given_name": "Marcus",
+        "mail": "marcus.hardt@kit.edu",
+        "name": "Marcus Hardt",
+        "ou": "SCC",
+        "preferred_username": "lo0018",
+        "sn": "Hardt",
+        "sub": "4cbcd471-1f51-4e54-97b8-2dd5177e25ec"
+    }
+
+    def test_kit(self):
+        info = UserInfo({'user': {'userinfo': self.input_kit}})
+        self.assert_name(info)
+        self.assertEqual(info.username, "lo0018")
+        self.assertEqual(info.email, "marcus.hardt@kit.edu")
+
+    def test_kit_id(self):
+        info = UserInfo({'user': {'userinfo': self.input_kit}})
+        self.assertEqual(info.unique_id, "4cbcd471-1f51-4e54-97b8-2dd5177e25ec@kit.edu")
+        self.assertEqual(info.eppn, "lo0018@kit.edu")
+
+    def test_kit_entitlements_valid(self):
+        info = UserInfo({'user': {'userinfo': self.input_kit}})
+        self.assertTrue(list(info.entitlement))
