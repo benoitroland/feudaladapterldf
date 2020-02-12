9556104a9f9d9e9a25055d8b37570eef757456ea
Marcus <marcus@hardt-it.de>
2020-02-12 15:19:38 +0100

    add caching and a cachedir

--- ldf_adapter/backend/bwidm.py
+++ ldf_adapter/backend/bwidm.py
@@ -10,7 +10,7 @@ from functools import reduce
 
 import requests
 import requests_cache
-requests_cache.install_cache(include_get_headers=True, expire_after=2)
+import os
 
 from urllib.parse import urljoin
 
@@ -18,6 +18,11 @@ from ..config import CONFIG
 from .. import utils
 
 logger = logging.getLogger(__name__)
+
+requests_cache_dir = '/tmp/feudalAdapterLDF_bwidm'
+if not os.path.isdir(requests_cache_dir):
+   os.makedirs(requests_cache_dir)
+requests_cache.install_cache(requests_cache_dir+'/requests_cache.sqlite', include_get_headers=True, expire_after=2)
 
 class BwIdmConnection:
     """Connection to the BWIDM API."""
