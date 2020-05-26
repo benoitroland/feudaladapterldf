2f23108d5410a94b010fe6f1c49a79a914e2fac6
Marcus <marcus@hardt-it.de>
2020-05-26 16:15:03 +0200

    bump version by one

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as me:
 
 setuptools.setup(
     name="ldf_adapter",
-    version="0.1.1",
+    version="0.1.2",
     author="Joshua Bachmeier",
     author_email="joshua.bachmeier@student.kit.edu",
     description="LDF Adapter to connect BWIDM and FEUDAL",
