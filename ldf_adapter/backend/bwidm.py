63b8093da152b1c8b91d1c7ab23fc22359d71653
Joshua Bachmeier <joshua.bachmeier@student.kit.edu>
2020-05-27 08:19:35 +0200

    Revert "catch AttributeError and log it"
    This reverts commit 19c53ca9ba17ef7083a1f22aa5164b29c813ecb0.

--- ldf_adapter/backend/bwidm.py
+++ ldf_adapter/backend/bwidm.py
@@ -276,13 +276,9 @@ class Group:
 
     def create(self):
         rsp = BWIDM.get('group-admin', 'create', CONFIG['backend.bwidm.service']['name'], self.name).json()
-        try:
-            if self.name != rsp.name:
-                logger.warning("Groupname changed from {} to {} by BWIDM".format(self.name, rsp.name))
-                self.name = rsp.name
-        except AttributeError as e:
-            logger.error(F"Did not obtain a json with group name back: {e}")
-            logger.error(F"What is got is {rsp}")
+        if self.name != rsp.name:
+            logger.warning("Groupname changed from {} to {} by BWIDM".format(self.name, rsp.name))
+            self.name = rsp.name
 
         self.id = rsp.id
 
