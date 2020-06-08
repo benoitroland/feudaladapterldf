30e60843c5a4794ee127c3cf99a4d5660e87000d
Joshua Bachmeier <joshua.bachmeier@student.kit.edu>
2020-06-08 16:14:24 +0200

    adapter: Also ensure existance of primary group

--- ldf_adapter/__init__.py
+++ ldf_adapter/__init__.py
@@ -261,7 +261,7 @@ class User:
 
         Create the groups on the service, if necessary.
         """
-        for group in filter(lambda grp: not grp.exists(), self.service_groups):
+        for group in filter(lambda grp: not grp.exists(), [self.service_user.primary_group] + self.service_groups):
             logger.info("Creating group {}".format(group.name))
             group.create()
 
