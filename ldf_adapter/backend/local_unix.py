7688ae9512831da2a9a81a7ae5945da5a3871133
Marcus <marcus@hardt-it.de>
2020-08-05 10:01:00 +0200

    make sure primary group is defined

--- ldf_adapter/backend/local_unix.py
+++ ldf_adapter/backend/local_unix.py
@@ -24,6 +24,7 @@ class User:
         self.name = make_shadow_compatible(userinfo.username)
         self.unique_id = userinfo.unique_id
         self.ssh_keys = [key['value'] for key in userinfo.ssh_keys]
+        self.primary_group = Group(userinfo.primary_group) 
         self.credentials = {}
 
     def exists(self):
