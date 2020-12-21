47dfd57612731c97c9ca719861f0a18cd4819de7
Marcus <marcus@hardt-it.de>
2020-12-21 16:45:52 +0100

    display loaded config file

--- ldf_adapter/config.py
+++ ldf_adapter/config.py
@@ -46,7 +46,7 @@ def reload():
     for f in files:
         if f.exists():
             files_read = CONFIG.read(f)
-            logger.debug(F"Read config from {files_read}")
+            logger.info(F"Read config from {files_read}")
             break
 
 # Load config on import
