a4066610d8466cd410e21529f8322807b38959c8
Marcus <marcus@hardt-it.de>
2020-12-22 16:08:41 +0100

    name_generator stub

---
+++ ldf_adapter/name_generators.py
@@ -0,0 +1,13 @@
+"""
+Generate useful user or group names
+"""
+
+# vim: foldmethod=indent : tw=100
+# pylint: disable=invalid-name, superfluous-parens
+# pylint: disable=logging-fstring-interpolation, logging-not-lazy, logging-format-interpolation
+# pylint: disable=raise-missing-from, missing-docstring, too-few-public-methods
+
+
+def generate_friendly_username(service_user, forbidden_names=None):
+    """Generate a friendly username"""
+    return "marcus"
