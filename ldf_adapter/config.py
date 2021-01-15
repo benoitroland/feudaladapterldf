e0489092196f957ae3f6148a3e75db2056da1a15
Marcus <marcus@hardt-it.de>
2021-01-15 14:50:27 +0100

    add support for globalconfig; add /etc/feudal/ldf_adapter.conf to list of config files to consider

--- ldf_adapter/config.py
+++ ldf_adapter/config.py
@@ -1,14 +1,27 @@
 import os
 import sys
 
+from globalconfig import globalconfig
 from configparser import ConfigParser
 from pathlib import Path
 import logging
 
+PARSE_CMDLINE_PARAMETERS = True
 if 'pytest' not in sys.modules:
+    PARSE_CMDLINE_PARAMETERS = False
+else:
+    try:
+        if globalconfig.config['parse_commandline_args']:
+            PARSE_CMDLINE_PARAMETERS = False
+    except KeyError as e:
+        PARSE_CMDLINE_PARAMETERS = True
+
+if PARSE_CMDLINE_PARAMETERS:
+    print ("WILL PARSE PARAMS")
     from ldf_adapter.cmdline_params import args
 
 CONFIG = ConfigParser()
+
 
 def reload():
     """Reload configuration from disk.
@@ -18,6 +31,7 @@ def reload():
     $LDF_ADAPTER_CONFIG
     ./ldf_adapter.conf
     ~/.config/feudal/ldf_adapter.conf
+    /etc/feudal/ldf_adapter.conf
 
     processing is stopped, once a give file is found
     """
@@ -29,7 +43,7 @@ def reload():
     logger = logging.getLogger(__name__)
     files = []
 
-    if 'pytest' not in sys.modules:
+    if PARSE_CMDLINE_PARAMETERS:
         files += [ Path(args.config_file) ]
 
     filename = os.environ.get("LDF_ADAPTER_CONFIG")
@@ -40,6 +54,7 @@ def reload():
         Path('ldf_adapter.conf'),
         Path.home()/'.config'/'ldf_adapter.conf',
         Path.home()/'.config'/'feudal'/'ldf_adapter.conf',
+        Path('/etc/feudal/ldf_adapter.conf')
     ]
     logger.debug("Using these config files: {} to find a suitable one".format(files))
 
