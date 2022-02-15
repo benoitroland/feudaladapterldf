ab018a2b4acbe78fd37b72ac2def7579a4ad1859
Marcus <marcus@hardt-it.de>
2022-02-15 15:44:42 +0100

    include pytest config in search path

--- ldf_adapter/config.py
+++ ldf_adapter/config.py
@@ -73,7 +73,8 @@ def reload():
         Path('ldf_adapter.conf'),
         Path.home()/'.config'/'ldf_adapter.conf',
         Path.home()/'.config'/'feudal'/'ldf_adapter.conf',
-        Path('/etc/feudal/ldf_adapter.conf')
+        Path('/etc/feudal/ldf_adapter.conf'),
+        Path('ldf_adapter/tests/feudal_adapter_pytest.conf')
     ]
 
     config_loaded = False
