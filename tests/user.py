e0339da8948309f8cfe2932553cf7df4bbb724e7
Marcus <marcus@hardt-it.de>
2022-02-16 19:57:25 +0100

    black

--- tests/user.py
+++ tests/user.py
@@ -5,10 +5,7 @@ from ldf_adapter import UserInfo, User
 import settings
 
 
-@pytest.mark.parametrize('data', settings.ALL_INPUT)
+@pytest.mark.parametrize("data", settings.ALL_INPUT)
 def test_get_status(user):
     # assert user.assurance_verifier() == True
-    assert user.get_status().attributes == {
-        "state": "not_deployed",
-        "message": "No message"
-    }
+    assert user.get_status().attributes == {"state": "not_deployed", "message": "No message"}
