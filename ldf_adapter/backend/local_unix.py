decfcfbabfb19f11a2388c76df9b3ff537456f59
Benoit Roland <benoit.roland@gmx.de>
2022-07-06 14:25:00 +0200

    version passing INPUT_SHADOW_COMPATIBLE tests

--- ldf_adapter/backend/local_unix.py
+++ ldf_adapter/backend/local_unix.py
@@ -451,28 +451,30 @@ def make_shadow_compatible(orig_word) -> str:
 
     if excess_chars > 0:
 
-        for n in range(len(fragments)):
+	    if len(fragments) == 1:
+            word = "__" + word[excess_chars+2:]
+        else:
+            for n in range(1, len(fragments)):
 
-            if excess_chars == 0: break
-            excess_chars += 2
-
-            for nchar in range (len(fragments[n])):
-
-                fragments[n] = fragments[n][1:]
-                excess_chars -= 1
                 if excess_chars == 0: break
+                excess_chars += 2
 
-            fragments[n] = ".." + fragments[n]
+                for nchar in range (len(fragments[n])):
+                    fragments[n] = fragments[n][1:]
+                    excess_chars -= 1
+                    if excess_chars == 0: break
 
-    if orig_excess_chars > 0:
+                fragments[n] = ".." + fragments[n]
+
+    if orig_excess_chars > 0 and len(fragments) > 1:
 
         if excess_chars > 0:
             logger.error(f"User or group name is too long and could not be shortened: {word} ({len(word)})")
-            raise (ValueError)
-
-        else:
+	        raise (ValueError)
+	    else:
             word = "_".join(fragments)
             logger.warning(F"User or group name is too long and was shortened from {orig_word} ({len(orig_word)}) to {word} ({len(word)})")
 
     return word
+
 
