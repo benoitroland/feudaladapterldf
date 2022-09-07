34bc675fcb206e19cfdf6de83bafb3be693b310a
Diana Gudu <diana.gudu@kit.edu>
2022-09-07 10:22:13 +0200

    remove redundant MockUserInfo definition in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,23 +8,3 @@ def userinfo(data):
     """Creates a UserInfo object from provided dict"""
     userinfo = UserInfo(data)
     yield userinfo
-
-
-class MockUserInfo:
-    """Mocks a UserInfo object to be passed to backend user
-    Only a few properties are necessary:
-        - unique_id
-        - username
-        - primary_group
-        - ssh_keys
-    """
-
-    def __init__(self, data):
-        self.unique_id = data.get("unique_id")
-        self.username = data.get("username")
-        self.primary_group = data.get("primary_group")
-        self.ssh_keys = data.get("ssh_keys")
-        self.family_name = data.get("family_name")
-        self.given_name = data.get("given_name")
-        self.full_name = data.get("full_name")
-        self.email = data.get("email")
